6dd5c0a6e67988c9bff742a515fb0d9d8c3bdaa3
KymCat <kohina1452@gmail.com>
2025-08-15 14:54:12 +0900

    repository connect first commit and push

---
+++ src/main/java/com/example/toDoList/controller/toDoListController.java
@@ -0,0 +1,10 @@
+package com.example.toDoList.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequiredArgsConstructor
+public class toDoListController {
+
+}
